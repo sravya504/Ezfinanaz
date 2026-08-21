b5e1025e172637286c29b75a3d61b744483c1fc7
sravya504 <n210307@rguktn.ac.in>
2026-08-21 09:59:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Signup from "./pages/Signup";
 
 import CustomerDashboard from "./pages/CustomerDashboard";
